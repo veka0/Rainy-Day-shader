60e1598864343cbeaa692d2914cd03aa62f4dee1
Veka0 <26331365+Veka0@users.noreply.github.com>
2023-04-22 11:46:47 +0200

    Add files via upload

---
+++ shaders/glsl/fragmentHeader.h
@@ -0,0 +1,7 @@
+#version 300 es
+
+// This file is used as a workaround, to enable extensions.
+
+#extension GL_EXT_shader_framebuffer_fetch : enable
+#extension GL_ARM_shader_framebuffer_fetch : enable
+#extension GL_ARM_shader_framebuffer_fetch_depth_stencil : require
